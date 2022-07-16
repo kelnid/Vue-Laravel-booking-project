79f089fe3571664bb7cadc85b864f28976282521
kelnid <xopoho2222@gmail.com>
2022-07-16 10:03:15 +0300

    first validation

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CountrySeeder::class,
             HotelSeeder::class,
             RoomSeeder::class,
+            RoleSeeder::class,
         ]);
     }
 }
